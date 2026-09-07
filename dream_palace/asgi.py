431752e513460cf8b9962398c05ad4162ecb5922
Guguei-465 <riakgugwey@gmai.com>
2026-09-07 16:56:25 +0300

    money

--- dream_palace/asgi.py
+++ dream_palace/asgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from django.core.asgi import get_asgi_application
-sys.path.append("/home/ryacksonfungo/www/dream_palace_backend")  
+sys.path.append("/home/ryacksonfungo/www/dream")  
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dream_palace_backend.settings")
 
 application = get_asgi_application()
