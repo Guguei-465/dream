0657edf52c93be21abdb829409ea39dc45309614
Guguei-465 <riakgugwey@gmai.com>
2026-09-07 16:51:27 +0300

    mony

--- dream_palace/wsgi.py
+++ dream_palace/wsgi.py
@@ -1,6 +1,7 @@
 import os
 import sys 
 from django.core.wsgi import get_wsgi_application
+sys.path.append("/home/ryacksonfungo/www/dream_palace_backend") 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dream_palace_backend.settings")
 
 application = get_wsgi_application()
