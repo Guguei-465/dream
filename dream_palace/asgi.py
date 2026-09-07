49f1276353812f53c343a81cb92cb327bc483206
Guguei-465 <riakgugwey@gmai.com>
2026-09-07 17:37:05 +0300

    updaye

--- dream_palace/asgi.py
+++ dream_palace/asgi.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-sys.path.append("/home/ryackson/www/dream")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dream_palace.settings")
+sys.path.append("/home/ryackson/www/ryackson_dream")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dream_palace_backend.settings")
 
 from django.core.asgi import get_asgi_application
 application = get_asgi_application()
--- dream_palace/wsgi.py
+++ dream_palace/wsgi.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 # ✅ Path to where your project FOLDER lives (CONTAINS dream_palace/)
-sys.path.append("/home/ryacksonfungo/www/dream")
+sys.path.append("/home/ryackson/www/dream")
 
 # ✅ NAME OF THE FOLDER THAT HAS settings.py → dream_palace.settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dream_palace.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dream_palace_backend.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
